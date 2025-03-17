95af35871b8e8891ded0b14a3021d2d68f343238
leey311 <wwyl1@naver.com>
2025-03-17 16:48:58 +0900

    init

---
+++ src/main/java/com/ll/helphofs/HelphufsApplication.java
@@ -0,0 +1,13 @@
+package com.ll.helphofs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelphufsApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(HelphufsApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/com/ll/helphofs/HelphufsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.helphofs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelphufsApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
